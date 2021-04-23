9237a379c4bef3bb510c135a20b1d4f9537d53ca
nanqic <nanqic@163.com>
2021-04-23 18:23:38 +0800

    修复了按分类查询

--- src/main/java/com/shine/mapper/MySaleMapper.java
+++ src/main/java/com/shine/mapper/MySaleMapper.java
@@ -17,17 +17,14 @@ public interface MySaleMapper {
     @Insert("INSERT INTO t_sale(pid,rid,sold_amount) VALUES(#{pid},#{rid},#{soldAmount}) ON DUPLICATE KEY UPDATE sold_amount=sold_amount+VALUES(sold_amount)")
     int insertOrUpdate(int pid, int rid, int soldAmount);
 
-    @Select("SELECT `t_region`.`name`, sum( `t_sale`.`sold_amount` ) AS `amount` FROM ( `t_sale` JOIN `t_region` ON (( `t_region`.`rid` = `t_sale`.`rid` ))) \n" +
-            "WHERE\n" +
-            "\t`t_sale`.`pid` IN (\n" +
-            "\tSELECT\n" +
-            "\t\t`t_product`.`pid` \n" +
-            "\tFROM\n" +
-            "\t\t`t_product` \n" +
-            "\tWHERE\n" +
-            "\t( `t_product`.`sort_id` = #{sid} AND t_sale.update_time BETWEEN #{startTime} AND #{endTime} )) \n" +
-            "GROUP BY\n" +
-            "\t`t_region`.`name`")
+    /**
+     *
+     * @param sid 分类id
+     * @param startTime 下单时间
+     * @param endTime 结束时间
+     * @return AnalysisVO[] 返回名称和数量结果集
+     */
+    @Select("SELECT r.name, SUM(o.amount) as amount FROM t_order o JOIN t_product p ON o.pid=p.pid JOIN t_region r ON r.rid=o.rid WHERE p.pid IN (SELECT pid FROM t_product WHERE sort_id = #{sid}) AND o.order_time BETWEEN #{startTime} AND #{endTime} GROUP BY r.`name`")
     AnalysisVO[] getAnaBySid(Byte sid, String startTime, String endTime);
 
     /**
@@ -35,9 +32,9 @@ public interface MySaleMapper {
      * @param rid 地区id
      * @param startTime 订单的开始时间
      * @param endTime 结束时间
-     * @return
+     * @return AnalysisVO[] 返回名称和数量结果集
      *
     */
-    @Select("SELECT st.`name`,rid,SUM(o.amount) AS amount FROM t_order o JOIN t_product p ON o.pid=p.pid JOIN t_sort st ON p.sort_id=st.sid  WHERE o.`status`=2 AND rid=#{rid} AND o.order_time BETWEEN #{startTime} AND #{endTime} GROUP BY `name`")
+    @Select("SELECT st.`name`,SUM(o.amount) AS amount FROM t_order o JOIN t_product p ON o.pid=p.pid JOIN t_sort st ON p.sort_id=st.sid  WHERE o.`status`=2 AND rid=#{rid} AND o.order_time BETWEEN #{startTime} AND #{endTime} GROUP BY `name`")
     AnalysisVO[] getAnaByRegion(Byte rid, String startTime, String endTime);
 }
